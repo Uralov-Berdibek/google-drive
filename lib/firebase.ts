809f081b20aceb528fcbf107aef261ae1fee6e42
berdibek-2002 <uralov0212@gmail.com>
2024-01-26 08:32:15 +0500

    version 1.0.6

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,18 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "drive-430a3.firebaseapp.com",
-  projectId: "drive-430a3",
-  storageBucket: "drive-430a3.appspot.com",
-  messagingSenderId: "392692541504",
-  appId: "1:392692541504:web:de147bfb8031d0ff7ad7a5"
-};
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: 'drive-430a3.firebaseapp.com',
+	projectId: 'drive-430a3',
+	storageBucket: 'drive-430a3.appspot.com',
+	messagingSenderId: '392692541504',
+	appId: '1:392692541504:web:de147bfb8031d0ff7ad7a5',
+}
 
-!getApps().length ? initializeApp(firebaseConfig) : getApp();
+!getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore()
+const storage = getStorage()
 
-export {db}
+export { db, storage }
