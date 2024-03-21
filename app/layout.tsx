13994cd7eb89fe34ed2d37c98ff63a5660762167
berdibek-2002 <uralov0212@gmail.com>
2024-03-21 10:34:33 +0500

    version 1.1.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 			<html lang='en' suppressHydrationWarning>
 				<body className={inter.className}>
 					<ThemeProvider
